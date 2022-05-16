17dfc6ef34129bc085fe205e97a3c55385449caf
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2022-05-16 13:22:17 -0300

    mais exemplos para rotas

--- src/app/pomodoro/pomodoro.component.ts
+++ src/app/pomodoro/pomodoro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { QuoteserviceService } from './quoteservice.service';
 
@@ -8,18 +9,18 @@ import { QuoteserviceService } from './quoteservice.service';
   styleUrls: ['./pomodoro.component.css']
 })
 export class PomodoroComponent implements OnInit {
-  qod: Observable<{q: string, a: string, h: string}> = new Observable<{q: string, a: string, h: string}>();
+  qod: Observable<{ q: string, a: string, h: string }> = new Observable<{ q: string, a: string, h: string }>();
 
-  tempos: number[] = [10, 5*60, 25*60, 5*60, 25*60, 45*60];
+  tempos: number[] = [10, 5 * 60, 25 * 60, 5 * 60, 25 * 60, 45 * 60];
   tempo_corrente: number;
   tempo: number = 0;
   ligado: boolean = false;
-  
+
   botao: number = 0;
 
   alarme: HTMLAudioElement;
 
-  constructor(private quote: QuoteserviceService) {
+  constructor(private quote: QuoteserviceService, private router: Router) {
     this.tempo_corrente = this.tempos[this.tempo];
 
     this.alarme = new Audio("https://upload.wikimedia.org/wikipedia/commons/5/5c/Singapore_Public_Warning_System_siren.ogg");
@@ -40,8 +41,8 @@ export class PomodoroComponent implements OnInit {
     }, 1000);
   }
 
-  checaTempo() : void {
-    if(this.tempo_corrente <= 0) {
+  checaTempo(): void {
+    if (this.tempo_corrente <= 0) {
       this.ligado = false;
       this.botao = 0;
       this.tempo++;
@@ -49,18 +50,18 @@ export class PomodoroComponent implements OnInit {
 
       this.alarme.play();
 
-      if(this.tempo == this.tempos.length - 1) this.tempo = -1;
+      if (this.tempo == this.tempos.length - 1) this.tempo = -1;
     }
 
 
   }
 
-  botaoClicado(texto: string) : void {
+  botaoClicado(texto: string): void {
     this.ligado = !this.ligado;
 
     this.alarme.pause();
 
-    switch(this.botao){
+    switch (this.botao) {
       case 0: this.botao = 1; break;
       case 1: this.botao = 2; break;
       case 2: this.botao = 1; break;
@@ -68,9 +69,14 @@ export class PomodoroComponent implements OnInit {
     }
   }
 
-  corrige(data: {pos: number, valor: number}) : void {
+  corrige(data: { pos: number, valor: number }): void {
     this.tempos[data.pos] = data.valor;
 
     console.log(this.tempos);
+  }
+
+  mudandoRotaComCodigo() {
+    // Algum processamento prévio
+    this.router.navigate(['/outra']);
   }
 }
